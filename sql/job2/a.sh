d1d1245ff1b5357b005815b2baabc2843cb16f65
hulichao <jsu_hlc@163.com>
2020-11-04 00:12:33 +0800

    add

--- sql/job2/a.sh
+++ sql/job2/a.sh
@@ -1,3 +1,2 @@
 dt=$(date "+%Y%m%d")
-hive -e \
-"load data local inpath '/user_clicks/$dt' overwrite into table mydb.user_clicks partition(dt=$dt)"
+hive -e "load data local inpath '/user_clicks/$dt' overwrite into table mydb.user_clicks partition(dt=$dt)"
--- sql/job2/b.sh
+++ sql/job2/b.sh
@@ -1,4 +1,2 @@
 dt=$(date "+%Y%m%d")
-hive -e \
-"insert overwrite table mydb.user_info 
-select count(distinct id) as active_num,$dt from mydb.user_clicks"
+hive -e "insert overwrite table mydb.user_info select count(distinct id) as active_num,$dt from mydb.user_clicks"
