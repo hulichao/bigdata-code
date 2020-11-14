613c40e7c9e56fdfdcade8f1b5f8ed34c81ff642
hulichao <jsu_hlc@163.com>
2020-11-14 21:43:46 +0800

    kafka mv to bigdata-kafka

--- src/main/java/com/hoult/zk/Demo.java
+++ src/main/java/com/hoult/zk/Demo.java
@@ -5,7 +5,7 @@ import org.I0Itec.zkclient.ZkClient;
 public class Demo {
     public static void main(String[] args) {
         //先获取到zkClient对象，client与zk集群通信端口是2181
-        final ZkClient zkClient = new ZkClient("linux121:8521");
+        final ZkClient zkClient = new ZkClient("centos:8281");
         System.out.println("zkClient is ready");
         //创建节点
         zkClient.createPersistent("/zk-client/test/test2", true);
