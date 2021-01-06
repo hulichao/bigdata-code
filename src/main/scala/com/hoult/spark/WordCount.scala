d7e4bc90aa31f91dc2b6470a29ecf07ef7876b87
hulichao <hulichao@duxiaoman.com>
2021-01-06 11:52:49 +0800

    add

--- src/main/scala/com/hoult/spark/WordCount.scala
+++ src/main/scala/com/hoult/spark/WordCount.scala
@@ -7,7 +7,7 @@ object WordCount {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setMaster("local").setAppName("WordCount")
     val sc = new SparkContext(conf)
-    // val lines: RDD[String] = sc.textFile("hdfs://linux121:9000/wcinput/wc.txt")
+//     val lines: RDD[String] = sc.textFile("hdfs://linux121:8899/wcinput/wc.txt")
     val lines: RDD[String] = sc.textFile("data/spark/wc.txt")
     // val lines: RDD[String] = sc.textFile("data/wc.dat")
     lines.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_).collect().foreach(println)
