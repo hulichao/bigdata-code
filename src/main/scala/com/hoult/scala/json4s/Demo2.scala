762f69c675d077b3ff3fbb3e9e189947dd598c33
hulichao <hulichao@duxiaoman.com>
2021-02-21 18:32:55 +0800

    json4s

---
+++ src/main/scala/com/hoult/scala/json4s/Demo2.scala
@@ -0,0 +1,19 @@
+package com.hoult.scala.json4s
+import org.json4s.NoTypeHints
+import org.json4s.native.Serialization
+
+
+object Demo2 {
+  def main(args: Array[String]): Unit = {
+    // 11.对象转json
+    val jsonMap = ("name" -> "joe")
+
+//    implicit val f = org.json4s.DefaultFormats
+
+    implicit val formats = Serialization.formats(NoTypeHints)
+    val str = Serialization.write(jsonMap)
+    println(str)
+
+
+  }
+}
