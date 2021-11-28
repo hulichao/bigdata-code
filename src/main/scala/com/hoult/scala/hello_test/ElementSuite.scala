88886033d9bb72bfbb3f737579bd53327e56e5f9
hulichao <jsu_hlc@163.com>
2021-11-28 16:12:05 +0800

    diff

--- src/main/scala/com/hoult/scala/hello_test/ElementSuite.scala
+++ src/main/scala/com/hoult/scala/hello_test/ElementSuite.scala
@@ -1,5 +1,14 @@
 package com.hoult.scala.hello_test
+import org.scalatest.funsuite.AnyFunSuite
 
-class ElementSuite {
+class ElementSuite extends AnyFunSuite{
+  test("elem result should have passed width") {
+    assert(2 == 1)
+  }
 
+  test("") {
+    assert(1 == 3)
+  }
 }
+
+
